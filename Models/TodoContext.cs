aec113e93ebd0116cd03d9aa777dfb879f0101f9
vDuduardo <eduardo.m.verbinen@exxonmobil.com>
2022-04-08 11:54:54 -0300

    Add project files.

---
+++ Models/TodoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace to_do_list.Models
+{
+    public class TodoContext : DbContext   
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
---
+++ Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace to_do_list.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
